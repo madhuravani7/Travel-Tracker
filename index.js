758b4348de418eedfce4e9188b6a255bf727eaad
madhuravani7 <madhubudige67@gmail.com>
2024-05-03 23:02:37 +1000

    index.js finished completely

--- index.js
+++ index.js
@@ -38,8 +38,9 @@ app.post("/add", async (req, res)=>{
   try {
     const result = await db.query(
       "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%'",
-      [input]
+      [input.toLowerCase]
     );
+    //Changing to lower case when checking.
 
     const data = result.rows[0];
     const countryCode = data.country_code;
